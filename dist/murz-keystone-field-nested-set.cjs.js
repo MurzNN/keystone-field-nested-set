1d187e9db440eb833ce9b260129fcf6675f0b84a
Alexey Murz Korepov <MurzNN@gmail.com>
2023-03-17 18:08:45 +0300

    Moving back to the main "keystone-field-nested-set" namespace

--- dist/murz-keystone-field-nested-set.cjs.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-if (process.env.NODE_ENV === "production") {
-  module.exports = require("./murz-keystone-field-nested-set.cjs.prod.js");
-} else {
-  module.exports = require("./murz-keystone-field-nested-set.cjs.dev.js");
-}
